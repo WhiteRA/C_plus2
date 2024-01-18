6e634b7c9a6fa597bdfdeaf5ecddc06575bcfd97
mr.White <whitera_@mail.ru>
2024-01-19 00:43:15 +1000

    +

---
+++ L3/ConsoleApplication1/t1.cpp
@@ -0,0 +1,12 @@
+﻿
+#include <iostream>
+
+int main()
+{
+    int a;
+    std::cout << "Введите число\n";
+    std::cin >> a;
+    std::cout << a*a << " = Квадрат числа\n";
+}
+
+
---
+++ L3/t2/t2.cpp
@@ -0,0 +1,21 @@
+﻿
+
+#include <iostream>
+
+int main() {
+	int productCost = 6400;
+	int deliveryCost = 350;
+	int discount = 700;
+
+	int price = productCost + deliveryCost - discount;
+
+	std::cout << "Стоимость товара: \n";
+	std::cin >> productCost;
+	std::cout << "Cтоимость доставки: \n";
+	std::cin >> deliveryCost;
+	std::cout << "Скидка: \n";
+	std::cin >> discount;
+	std::cout << "---------\n";
+	std::cout << "Итого: " << price << "\n";
+
+}
