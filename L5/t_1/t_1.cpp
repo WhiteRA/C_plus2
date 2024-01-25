c436de5ec6ee0c2fa6a6ddf148b8d9e6e581ac5d
mr.White <whitera_@mail.ru>
2024-01-25 23:23:19 +1000

    +

---
+++ L5/t_1/t_1.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main()
+{
+    int a;
+    int b;
+    cout << "Самолёта :\n";
+    cin >> a;
+    cout << "Высота :\n";
+    cin >> b;
+    if (750 < a < 850)
+    {
+        if (9000 < b < 9500) {
+            cout << "Правильным эшелоном летит самолёт\n";
+        }
+        else
+        {
+            cout << "Неравильным эшелоном летит самолёт\n";
+        }
+    }
+    else
+    {
+        cout << "Неравильным эшелоном летит самолёт\n";
+    }
+}
+
---
+++ L5/t_2/t_2.cpp
@@ -0,0 +1,19 @@
+﻿
+
+#include <iostream>
+
+int main() {
+    int day;
+
+    std::cout << "Введите номер дня: ";
+    std::cin >> day;
+
+    if ((day >= 1 && day <= 5) || (day >= 8 && day <= 10) || day % 7 == 6 || day % 7 == 0) {
+        std::cout << "Выходной день" << std::endl;
+    }
+    else {
+        std::cout << "Рабочий день" << std::endl;
+    }
+
+    return 0;
+}
