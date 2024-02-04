53d93deeac28260b1096e7591d7fac00dd8c9ec5
mr.White <whitera_@mail.ru>
2024-02-04 23:45:51 +1000

    +

---
+++ L6/t_1/t_1.cpp
@@ -0,0 +1,19 @@
+﻿
+
+#include <iostream>
+
+int main()
+{	
+	setlocale(LC_ALL, "Russian");
+
+	std::string message;
+    std::cout << "Введите команду\n";
+	std::cin >> message;
+	while (message != "Да")
+	{
+		std::cout << "Не верно\n";
+		std::cin >> message;
+	}
+	std::cout << "Хорошо\n";
+}
+
