3ba9822ba1cd036fa13b02264be4da4bf5bb652b
Aleksandr Parhomenko <whitera_@mail.ru>
2024-02-08 13:17:41 +1000

    +

--- L8/t_3/t_3.cpp
+++ L8/t_3/t_3.cpp
@@ -5,13 +5,18 @@ int main()
 {
     setlocale(LC_ALL, "Russian");
     int pCube, cCube, cSet;
-    float pСhunk, x,y,z;
- 
+    float pСhunk, x, y, z;
+
+    std::cout << "X ";
+    std::cin >> x;
+    std::cout << "\nY ";
+    std::cin >> y;
+    std::cout << "\nZ ";
+    std::cin >> z;
+
     pCube = 5 * 5 * 5;
     pСhunk = x * y * z;
     cCube = pСhunk / pCube;
-    cSet = cCube / 27; // нужно посчитать колличество кубов в большом кубе и посчитать колличество больших кубов
-    std::cout << "Hello World!\n";
+    cSet = cCube / 27;
+    std::cout << "Площадь одного кубика - " << pCube << "\nПлощадь бруска - " << pСhunk << "\nКолличество кубиков в бруске - " << cCube << "\nКолличество полный наборов - " << cSet;
 }
-
-
