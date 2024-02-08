9a2f02b1de4d76b2dc94c3ffd0775fc773215b05
mr.White <whitera_@mail.ru>
2024-02-08 23:50:31 +1000

    +

--- L9/t_2/t_2.cpp
+++ L9/t_2/t_2.cpp
@@ -11,10 +11,10 @@ int main() {
     int countWord = 0;
 
     std::cout << "\n В строке " << str1 << " ищем " << str2;
-
+    bool feach = true;
     for (int i = 0; i < str1.length(); i++)
     {
-        bool feach = true;
+        
         for (int j = 0; j < str2.length(); j++)
         {
             if (str2[j] == str1[i + j]) {
@@ -23,10 +23,10 @@ int main() {
             else
                 feach = false;
         }
-        if (feach) {
-            countWord++;
-        }
+       
     }
-
-    std::cout << "\n " << countWord;
+    if (feach) {
+        std::cout << "\nYES ";
+    }
+    
 }
