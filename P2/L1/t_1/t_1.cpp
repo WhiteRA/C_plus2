ad250abd7a6215d1eb067d5a4af0257d927c1cbe
mr.White <whitera_@mail.ru>
2024-02-14 01:00:42 +1000

    +

---
+++ P2/L1/t_1/t_1.cpp
@@ -0,0 +1,21 @@
+﻿
+
+#include <iostream>
+
+int main()
+{
+    std::string str1;
+    std::string secName[10];
+    for (int i = 0; i < (sizeof(secName) / sizeof(*secName)); i++)
+    {
+        std::cout << "Ввод фамилии\n";
+        std::cin >> str1;
+        secName[i] = str1;
+    }
+
+    int n;
+    std::cout << "Ввод номера крвартиры\n";
+    std::cin >> n;
+    std::cout << secName[n];
+}
+
