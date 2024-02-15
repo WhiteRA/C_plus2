87cab6a601eb313bb2d7389eb0695a23367fd82b
mr.White <whitera_@mail.ru>
2024-02-15 21:39:08 +1000

    +

---
+++ P2/L2/t_1/t_1.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int n, j;
+    int l = 0;
+    cout << "\n Размер - ";
+    cin >> n;
+
+    vector<int> w(n);
+    for (int i = 0; i < w.size(); i++)
+    {
+        cout << "\n Ввод - ";
+        cin >> w[i];
+    }
+
+    cout << "\n Удалить - ";
+    cin >> j;
+
+    vector<int> result;
+    for (int num : w) {
+        if (num != j) {
+            result.push_back(num);
+            cout << num << " ";
+        }
+    }
+
+
+    return 0;
+}
---
+++ P2/L2/t_2/t_2.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    vector<float> prices{ 2.5, 4.25, 3.0, 10.0 };
+    vector<int> items{ 1, 1, 0, 3 };
+    float i = 0;
+    for (int num : items)
+    {
+        i += prices[num];
+    }
+    cout << i << " ";
+    std::cout << typeid(i).name() << std::endl;
+}
