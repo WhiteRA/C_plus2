97f38bcaa9f3b11cf2f352064884abee0cd53f51
mr.White <whitera_@mail.ru>
2024-02-18 16:03:11 +1000

    +

---
+++ P2/L4/t_1/t_1.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	int a[9] = { -2,1,-3,4,-1,2,1,-5,4 };
+	/*
+	ВООБЩЕ НЕ ПОНЯЛ ЗАДАНИЕ.
+	*/
+
+}
---
+++ P2/L4/t_2/t_2.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	int a[] = { 2, 7, 11, 15 };
+	int sym = 9;
+	for (int i = 0; i < sizeof(a) / sizeof(*a); i++)
+	{
+		for (int j = 0; j < sizeof(a) / sizeof(*a)-1; j++)
+		{
+			if (a[i] + a[j + 1] == sym) {
+				cout << a[j] << " + " << a[j + 1] << endl;
+			}
+		}
+	}
+}
